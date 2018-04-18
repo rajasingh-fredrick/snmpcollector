1b453c765b9fca05ad0333c69e3b7bf84fb288d9
NAGARATHINAM, RAJASINGH (rn1768) <rn1768@att.com>
2018-04-17 20:38:44 -0400

    Updated

--- src/index.js
+++ src/index.js
@@ -8,6 +8,10 @@ app.get('/', function(request, response) {
   response.send('Hello World!')
 })
 
+app.get('/index', function(request, response) {
+  response.send('Hello World Rajasingh!')
+})
+
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
 })
