32c0dc7ae7d8eb86774de490ba7228ee940f92a5
NAGARATHINAM, RAJASINGH (rn1768) <rn1768@att.com>
2018-04-18 10:27:57 -0400

    Updated

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.get('/index', function(request, response) {
 
 app.get('/device', function(request, response) {
 	
-	if (request.method == 'GET') {
+	if (request.method == 'POST') {
         var body = '';
 
         request.on('data', function (data) {
@@ -33,6 +33,18 @@ app.get('/device', function(request, response) {
             // use post['blah'], etc.
         });
     }
+	else {
+		var queryData = url.parse(request.url, true).query;
+		  response.writeHead(200, {"Content-Type": "text/plain"});
+
+		  if (queryData.username) {
+		    // user told us their name in the GET request, ex: http://host:8000/?name=Tom
+		    response.end('Hello ' + queryData.username + '\n');
+
+		  } else {
+		    response.end("Hello World\n");
+		  }
+	}
 	
 })
 
